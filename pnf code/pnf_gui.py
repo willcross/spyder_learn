50f0e581c9e79ce09eb727a7224d6a894395812c
weicai <weicai.zhang@harman.com>
2019-06-13 20:27:30 +0800

    version 1 released and advertized

--- pnf code/pnf_gui.py
+++ pnf code/pnf_gui.py
@@ -54,35 +54,50 @@ class App():
 #        y = (hs/2) - (250/2)
 #        self.root.geometry('%dx%d+%d+%d' % (940, 250, x, y))
 #        
-#        self.root.resizable(0,0)
+        self.root.resizable(0,0)
 #        self.label = tk.Label(self.root,text="作者微信 canbeshine",width=20, height=5,bg="white", font=("Arial", 20,'bold'))
 #        self.label.pack(fill="both", expand=True)
 ##        self.update_clock(time.clock())
 #        self.root.mainloop()
-        tk.Label(self.root, text = "步骤一 载入数据：").grid(row = 0, column = 0)
-        tk.Label(self.root, text = "方式一 输入品种代码：").grid(row = 0, column = 1)
-        tk.Label(self.root, text = "或者").grid(row = 0, column = 2)
-        tk.Label(self.root, text = "方式二 载入离线数据").grid(row = 0, column = 3)
+        #%%
+        group = tk.LabelFrame(self.root,height=265)
+        group.pack(side = 'left')
+        tk.Label(group, text = "步骤一 载入数据：").grid(row = 0, column = 0)
+        tk.Label(group, text = "方式一 输入品种代码：").grid(row = 0, column = 1)
+        tk.Label(group, text = "或者").grid(row = 0, column = 2)
+        tk.Label(group, text = "方式二 载入离线数据").grid(row = 0, column = 3)
        
-        tk.Label(self.root, text = "步骤二 输入价格间距").grid(row = 2, column = 0) 
+        tk.Label(group, text = "步骤二 输入价格间距(或不输入)").grid(row = 2, column = 0) 
         
         
-        self.e1 = tk.Entry(self.root)#stock number
-        self.e2 = tk.Entry(self.root)#price step
+        self.e1 = tk.Entry(group)#stock number
+        self.e2 = tk.Entry(group)#price step
         
         self.e1.grid(row = 1, column = 1, padx = 10, pady = 5)
         self.e2.grid(row = 2, column = 1, padx = 10, pady = 5)
         
-        tk.Label(self.root, text = "步骤三  点击 ‘画图’ 按钮").grid(row = 3, column = 0)
+        tk.Label(group, text = "步骤三  点击 ‘画图’ 按钮").grid(row = 3, column = 0)
         
  
 
         self.path = ''
         self.code = ''
-        tk.Button(self.root, text = "载入数据", width = 10, command = self.load_data)\
+        tk.Button(group, text = "载入数据", width = 10, command = self.load_data)\
                         .grid(row = 1, column = 3, sticky = "w", padx = 10, pady = 5)
-        tk.Button(self.root, text = "画图", width = 10, command = self.show)\
+        tk.Button(group, text = "画图", width = 10, command = self.show)\
                         .grid(row = 3, column = 1, sticky = "e", padx = 10, pady = 5)  #退出就直接调用窗口的 quit() 方法 
+        #%%
+        
+        
+        
+        group2 = tk.LabelFrame(self.root,text='提供威科夫看盘/选股选品种指标,交易策略',height=265)
+        group2.pack(side = 'right')
+        photo = tk.PhotoImage(file = 'qrcode.gif')
+        imgLabel = tk.Label(group2, image = photo)
+        imgLabel.pack(side ='right')
+#        frame1.pack()
+#        frame2.pack()
+#        imgLabel.pack(side = "right")
         self.root.mainloop()
     def load_data(self):
         try:
@@ -104,10 +119,10 @@ class App():
             step = None
         else: 
             try :
-                if type(int(self.step))==int :
+                if type(float(self.step))==float :
                     step = int(self.step)
             except:
-                tkinter.messagebox.showinfo('提示','请输入整数间距或不输入')
+                tkinter.messagebox.showinfo('提示','请输入数字间距或不输入')
             
         
         print(r"code %s" % self.code)
